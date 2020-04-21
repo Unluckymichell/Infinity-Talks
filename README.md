d080c5ef06ff076a7495649dcbd6975d1d51b0dd
Unluckymichell <39999330+Unluckymichell@users.noreply.github.com>
2020-04-21 21:13:31 +0200

    Upload of private version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Infinity-Talks
- Coming soon
+# Infinity-talks
+ A discord bot cleaning up open talks.
+
+## Features
+ - Automaticaly keeps one empty talk (discord voice channel) in a certain category
+ - Users in the talk can lock their talk to stop other people from joining in
+ - Custom prefix, category name, and even custom naming for talks
+ - Full help menu for all posible commands (default prefix: __t!help__)
+
+## Commands
+ All commands are documeted in the help menu of the bot!
+ This section will be updated soon.
+
+`PackageName: infinity-talks` 
+`Version: 1.0.0` 
---
+++ database.js
@@ -0,0 +1,90 @@
+// Imports
+const sqlite3 = require('sqlite3').verbose();
+var db = null;
+
+function init(cconsole, db_path, callback) {
+    console = cconsole;
+    db = new sqlite3.Database(db_path, err => {
+        if (err) console.error(err);
+        db.run(`
+        CREATE TABLE IF NOT EXISTS "data" (
+            "id"	        TEXT NOT NULL,
+            "prefix"	    TEXT NOT NULL,
+            "servername"    TEXT NOT NULL,
+            "lang"	    TEXT NOT NULL,
+            "categoryName"	BLOB NOT NULL,
+            "talkNameRules"	BLOB NOT NULL,
+            "password"      INTEGER NOT NULL,
+            PRIMARY KEY("id")
+        );`, function (err) {
+            if (err) console.error(err);
+            callback();
+        });
+    });
+}
+
+function getGuildSettings(qid, defaultGs, guildName, callback) {
+    db.get(`
+        SELECT *
+        FROM "data"
+        WHERE id = ?;
+    `, [qid], function (err, gs) {
+        if(err) console.error(err);
+        if(gs) callback(gs);
+        else {
+            db.run(`
+                INSERT INTO "data"
+                VALUES (?, ?, ?, ?, ?, ?, ?);
+            `, [qid, defaultGs.prefix, guildName, defaultGs.lang, defaultGs.categoryName, defaultGs.talkNameRules, defaultGs.password], function (err) {
+                if(err) console.error(err);
+                callback(defaultGs);
+            });
+        }
+    });
+}
+
+function changeGuildSettings(qid, key, value, callback) {
+    db.run(`
+        UPDATE "data"
+        SET ${key} = ?
+        WHERE id = ?;
+    `, [value, qid], function (err) {
+        if(err) console.error(err);
+        callback();
+    });
+}
+
+function clearDatabase(callback) {
+    db.run(`
+        DELETE FROM "data";
+    `, function (err) {
+        if(err) console.error(err);
+        callback(this.changes);
+    });
+}
+
+function output_database() {
+    var padBy = 18;
+    console.log(`┌${"─".repeat(padBy)}┬${"─".repeat(padBy)}┬${"─".repeat(padBy)}┬${"─".repeat(padBy)}┬${"─".repeat(padBy)}┬${"─".repeat(padBy)}┬${"─".repeat(padBy)}┐`);
+    console.log(`│${"id".padEnd(padBy)}│${"prefix".padEnd(padBy)}│${"servername".padEnd(padBy)}│${"lang".padEnd(padBy)}│${"categoryNames".padEnd(padBy)}│${"talkNameRules".padEnd(padBy)}│${"password".padEnd(padBy)}│`);
+    console.log(`├${"─".repeat(padBy)}┼${"─".repeat(padBy)}┼${"─".repeat(padBy)}┼${"─".repeat(padBy)}┼${"─".repeat(padBy)}┼${"─".repeat(padBy)}┼${"─".repeat(padBy)}┤`);
+    db.each(`
+        SELECT *
+        FROM "data"
+    `, function (err, row) {
+        if (err) console.error(err);
+        console.log(`│${row.id.padEnd(padBy)}│${row.prefix.padEnd(padBy)}│${row.servername.padEnd(padBy)}│${row.lang.padEnd(padBy)}│${row.categoryName.padEnd(padBy)}│${row.talkNameRules.padEnd(padBy).slice(0, padBy)}│${`${row.password}`.padEnd(padBy)}│`);
+    }, function (err, count) {
+        if (err) console.error(err);
+        console.log(`├${"─".repeat(padBy)}┴${"─".repeat(padBy)}┴${"─".repeat(padBy)}┴${"─".repeat(padBy)}┴${"─".repeat(padBy)}┴${"─".repeat(padBy)}┴${"─".repeat(padBy)}┘`);
+        console.log(`└ Rows: ${count}`);
+    });
+}
+
+module.exports = {
+    init,
+    getGuildSettings,
+    changeGuildSettings,
+    clearDatabase,
+    output_database
+}
