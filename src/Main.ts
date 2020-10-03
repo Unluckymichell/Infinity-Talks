42b1acc07afe13fc84acde89ec99528b25ce2098
Unluckymichell <39999330+Unluckymichell@users.noreply.github.com>
2020-10-03 13:26:59 +0200

    Fixed lots of bugs

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,12 @@
 export const projectRoot = require("path").join(__dirname, "..");
 import Eris from "eris";
-import {CategorySchema, GuildModel} from "./Database/models";
+import {GuildModel} from "./Database/models";
 import {EventCompressor} from "./Util/EventCompressor";
 import {LANG} from "./Language/all";
 import {Logger} from "./Util/Logger";
 import {ChannelNameGenerator} from "./Util/ChannelNameGen";
 import {DatabaseManager} from "./Database/Manager";
+import {Commands} from "./Commands";
 require("dotenv").config();
 const LOGGER = new Logger(__filename);
 
@@ -32,6 +33,13 @@ class Main {
             this.voiceChannelUpdate(oc);
         });
         this.bot.on("voiceChannelLeave", (_m, c) => this.voiceChannelUpdate(c));
+        this.bot.on("voiceStateUpdate", m => {
+            if (m.voiceState.channelID) {
+                var c = this.bot.getChannel(m.voiceState.channelID);
+                if (c.type == 2) this.voiceChannelUpdate(c);
+            }
+        });
+
 
         this.bot.connect();
     }
@@ -39,7 +47,7 @@ class Main {
     async voiceChannelUpdate(ch: Eris.VoiceChannel) {
         const guild = ch.guild;
         if (!ch.parentID) return; // If there is no category, ignore event
-        if (!(await this.ec.waitcomp(`vcu#${ch.parentID}`, 100))) return; // Compress spamed Events
+        if (!(await this.ec.waitcomp(`vcu#${ch.parentID}`, 300))) return; // Compress spamed Events
 
         var gInfo = await GuildModel.findOne({_dcid: guild.id}); // Request guild information from db
         if (!gInfo) gInfo = await new GuildModel({_dcid: guild.id}).save(); // Save default if not found
@@ -63,18 +71,16 @@ class Main {
 
         // Start processing channels
         var pos = 1;
+        LOGGER.debug(`Event(${guild.name}:${category.name})`);
         for (var i = 0; i < channels.length; i++) {
-            const channel = channels[i];
-            const userCount = channel.voiceMembers.size;
-            const locked = (channel.userLimit || 0) > 0;
-            const name = this.cng.build(
-                {pos: pos, num: pos, l: locked, ul: !locked},
-                catInfo.namingRule
-            );
+            var channel = channels[i]; // TODO: FTW is
+            var userCount = channel.voiceMembers.size;
+            var locked = (channel.userLimit || 0) > 0;
+            var name = this.cng.build({pos, locked}, catInfo.namingRule);
             if (i == channels.length - 1) {
                 if (userCount > 0) {
                     const newname = this.cng.build(
-                        {pos: pos + 1, num: pos + 1, l: false, ul: true},
+                        {pos: pos + 1, locked: false},
                         catInfo.namingRule
                     );
                     this.bot
@@ -82,17 +88,23 @@ class Main {
                             parentID: category.id,
                         })
                         .catch(err => console.error(err));
+                    LOGGER.debug(`new Channel(${newname})`);
                 }
-                if (locked && userCount != channel.userLimit) channel.edit({userLimit: userCount});
-                if (channel.name != name) channel.edit({name});
+                let edit = {bitrate: channel.bitrate, name, userLimit: locked ? userCount : 0};
+                if ((locked && channel.userLimit != userCount) || channel.name != name) {
+                    channel.edit(edit, "Talk Bot");
+                    LOGGER.debug(`[${channel.name}].edit(${JSON.stringify(edit)})`);
+                }
             } else {
                 if (userCount < 1) {
                     this.bot.deleteChannel(channel.id).catch(err => console.error(err));
-                    continue;
+                    LOGGER.debug(`[${channel.name}].delete()`);
                 } else {
-                    if (locked && userCount != channel.userLimit)
-                        channel.edit({userLimit: userCount});
-                    if (channel.name != `${pos}`) channel.edit({name: `${pos}`});
+                    let edit = {bitrate: channel.bitrate, name, userLimit: locked ? userCount : 0};
+                    if ((locked && channel.userLimit != userCount) || channel.name != name) {
+                        channel.edit(edit, "Talk Bot");
+                        LOGGER.debug(`[${channel.name}].edit(${JSON.stringify(edit)})`);
+                    }
                     pos++;
                 }
             }
--- src/Util/ChannelNameGen.ts
+++ src/Util/ChannelNameGen.ts
@@ -1,12 +1,10 @@
 export class ChannelNameGenerator {
     /**
      * Syntax samples:
+     * <if:$locked>🔒<else>🔓</if> Talk $pos
      *
-     * Conditinal Block: Talk $pos is <l>locked<l><ul>unlocked<ul>!
-     * Insert val: $VAL
-     *
-     * @param vars Object containint all availabel variables
-     * @param rule Base rule that will be converted
+     * @param vars Object containing all availabel variables
+     * @param rule Base rule string that will be converted
      */
     build(vars: {[key: string]: string | number | boolean}, rule: string) {
         var out = rule;
@@ -14,26 +12,51 @@ export class ChannelNameGenerator {
         for (const key in vars) {
             out = out.replace(new RegExp(`\\$${key}`, "g"), `${vars[key]}`);
         }
+
         // Blocks
-        for (const v in vars) {
-            const vv = vars[v];
-            if (typeof vv == "boolean") {
-                out = this.buildBlock(out, v, vv);
-            }
-        }
+        out = this.buildBlocks(out);
         return out;
     }
 
-    private buildBlock(str: string, name: string, bool: boolean) {
-        var list = str.split(`<${name}>`);
-        var active = false;
-        var out = "";
-        for (const s of list) {
-            if (active) {
-                if (bool) out += s;
-            } else out += s;
-            active = !active;
+    buildBlocks(str: string) {
+        const rest = str.split(/<if:.*?>.*?<\/if>/gi);
+        const blocks = [];
+        // Find if blocks
+        var reg = /<if:.*?>.*?<\/if>/gi,
+            res = reg.exec(str);
+        // Loop if blocks
+        while (null != res) {
+            // Replace else key
+            const block = res[0].replace("<else>", "%^&");
+            const sections = block.split(/<|>/gi);
+            var result1 = true;
+            // Check correct formation
+            if (sections.length == 5) {
+                sections.shift(); // Remove ""
+                sections.pop(); // Remove ""
+                sections.pop(); // Remove "/if"
+                var [, ...conditions1] = sections.shift()?.split(/:|&/gi);
+                // Loop and conditions
+                for (const condition1 of conditions1) {
+                    var conditions2 = condition1.split(/\|/gi);
+                    var result2 = false;
+                    for (const condition of conditions2) {
+                        var parts = condition.split("=");
+                        if (parts.length == 1 && parts[0] == "true") result2 = true;
+                        else if (parts.length == 2 && parts[0] == parts[1]) result2 = true;
+                    }
+                    if (!result2) result1 = false;
+                }
+                var out = sections[0].split("%^&");
+                blocks.push(result1 ? out[0] || "" : out[1] || "");
+            } else blocks.push("[Error: To many <>]");
+
+            res = reg.exec(str);
         }
-        return out;
+        var ret = "";
+        while (rest.length > 0 || blocks.length > 0) {
+            ret += (rest.shift() || "") + (blocks.shift() || "");
+        }
+        return ret;
     }
 }
