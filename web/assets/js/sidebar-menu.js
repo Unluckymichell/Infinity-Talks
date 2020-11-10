1ae444a8e91fe88423d69ac2988c6997bcf2577b
Unluckymichell <39999330+Unluckymichell@users.noreply.github.com>
2020-11-10 20:31:08 +0100

    Enabled minify option

--- web/assets/js/sidebar-menu.js
+++
@@ -1,8 +0,0 @@
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
-
-if (window.matchMedia('(max-width: 768px)').matches) {
-    $("#wrapper").toggleClass("toggled");
-}
